6cc2ab627c2beb71447d6c9bb3662e9da0a2f3ca
Joseph Smith <jcsmith1234@gmail.com>
2015-08-13 02:10:36 -0400

    Add Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813023311) do
+ActiveRecord::Schema.define(version: 20150813055636) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name",       limit: nil
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20150813023311) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "image",      limit: nil
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
